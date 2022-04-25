7d780c2bf4377c5c183376346747ccc53a32907a
Dean <deaneryfb@gmail.com>
2022-04-25 11:01:17 -0600

    Menu header, adding a class

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -35,12 +35,23 @@ exactly from the desktop to mobile  -->
                     <!-- </ul> -->
 
                     <!-- DNAMIC MENU  -->
-                    <?php
+                    <!-- <?php
                     wp_nav_menu(array(
                         'theme_location' => 'headerMenuLocation'
                     ));
                     
-                    ?>
+                    ?> -->
+
+                      <ul>
+                       
+                        <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li>
+                        <li><a href="#">Programs</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Campuses</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+
+
 
                 </nav>
                 <div class="site-header__util">
