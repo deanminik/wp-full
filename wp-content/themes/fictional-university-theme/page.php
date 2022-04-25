00cfa242397d66ee214aa33cf49d27d2086cba50
Dean <deaneryfb@gmail.com>
2022-04-24 20:16:54 -0600

    Breadcrumbs part 2 Parent and children level 1

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -4,7 +4,7 @@
 while (have_posts()) {
     the_post(); ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
@@ -15,12 +15,19 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <!-- <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span> -->
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php echo the_title();?></span>
-            </p>
-        </div>
+        <?php
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) { ?>
+            <!-- echo "I am a child page"; -->
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <!-- <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span> -->
+                    <a class="metabox__blog-home-link" href="<?php the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
+                </p>
+            </div>
+        <?php } ?>
+
+
 
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
