841df4ded922bd661634ee126053ac9cd728aeb3
Dean <deaneryfb@gmail.com>
2022-04-27 11:03:17 -0600

    single posts, metabox, title, content

--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -3,13 +3,34 @@ get_header();
 while (have_posts()) {
     # code...
     the_post(); ?>
-    <h2>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <!-- <p>Learn how the school of your dreams got started.</p> -->
+                <p>DON'T FORGET TO REPLACE ME LATER</p>
+            </div>
+        </div>
+    </div>
+    <div class="container container--narrow page section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <!-- <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span> -->
+                <a class="metabox__blog-home-link" href="<?php echo site_url('/blog');?>"><i class="fa fa-home" aria-hidden="true"></i> Blog home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', ') ?></span>
+            </p>
+        </div>
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+    <!-- <h2>
         <a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
         </a>
-    </h2>
-    <?php the_content() ?>
-    <hr>
+    </h2> -->
+    <!-- <?php the_content() ?> -->
+
 <?php }
 get_footer();
 ?>
