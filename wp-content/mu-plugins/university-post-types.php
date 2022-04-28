51a3ce5b6c2c782e6b3c7ee434084279e1652ac2
Dean <deaneryfb@gmail.com>
2022-04-28 01:45:07 -0600

    Installing Advanced Custom Fields

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -3,7 +3,7 @@ function university_post_types()
 {
     register_post_type('event', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,//add a space in the dashboard call posts but the url will be called event
