7cb13b264b38d773625fdfec6cf7e830b0e8f068
Dean <deaneryfb@gmail.com>
2022-04-27 21:26:34 -0600

    Display custom post types, using a new custom archive to gathering al event post and more

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,7 +2,9 @@
 function university_post_types()
 {
     register_post_type('event', array(
-        'public' => true, //add a space in the dashboard call posts but the url will be called event
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,//add a space in the dashboard call posts but the url will be called event
         'labels' => array(
             'name' => 'Events', //to change post to event in the dashboard 
             'add_new_item' => 'Add a new event',
@@ -15,6 +17,9 @@ function university_post_types()
     ));
     // To see more paremeters of register_post_type visit this web site 
     // https://developer.wordpress.org/reference/functions/register_post_type/ 
+    
+    //IMPORTANT: Remember if you add a new parameter inside the array you need to go to the
+    // setting-permalink and save 
 }
 add_action('init', 'university_post_types');
 ?>
