e6dce7563ad591f6ce781bfc805968335cb09329
Dean <deaneryfb@gmail.com>
2022-04-28 00:38:33 -0600

    The best way to use an excerpt, adding features to edit the custom post in WordPress

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -14,6 +14,7 @@ exactly from the desktop to mobile  -->
 </head>
 
 <!-- body class gives all these classes (home blog logged-in admin-bar customize-support ) -->
+
 <body <?php body_class(); ?>>
 
     <header class="site-header">
@@ -26,29 +27,29 @@ exactly from the desktop to mobile  -->
             <div class="site-header__menu group">
                 <nav class="main-navigation">
                     <!-- <ul> -->
-                        <!-- <li><a href="#">About Us</a></li> -->
-                        <!-- <li><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li> -->
-                        <!-- <li><a href="#">Programs</a></li> -->
-                        <!-- <li><a href="#">Events</a></li> -->
-                        <!-- <li><a href="#">Campuses</a></li> -->
-                        <!-- <li><a href="#">Blog</a></li> -->
+                    <!-- <li><a href="#">About Us</a></li> -->
+                    <!-- <li><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li> -->
+                    <!-- <li><a href="#">Programs</a></li> -->
+                    <!-- <li><a href="#">Events</a></li> -->
+                    <!-- <li><a href="#">Campuses</a></li> -->
+                    <!-- <li><a href="#">Blog</a></li> -->
                     <!-- </ul> -->
 
                     <!-- DNAMIC MENU  -->
                     <!-- <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'headerMenuLocation'
-                    ));
-                    
-                    ?> -->
+                            wp_nav_menu(array(
+                                'theme_location' => 'headerMenuLocation'
+                            ));
 
-                      <ul>
-                       
-                        <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li>
+                            ?> -->
+
+                    <ul>
+
+                        <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
-                        <li><a href="#">Events</a></li>
+                        <li <?php if (get_post_type() == 'event') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
                         <li><a href="#">Campuses</a></li>
-                        <li <?php if(get_post_type()=='post') echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/blog')?>">Blog</a></li>
+                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
                     </ul>
 
 
