b5d6e427c7e32d1f45df89ffa138798de6efd359
Dean <deaneryfb@gmail.com>
2022-04-30 18:21:38 -0600

    Creating relaionship between type post

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,27 +1,47 @@
 <?php
 function university_post_types()
-{
+{   //EVENT POST TYPE
     register_post_type('event', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
-        'public' => true,//add a space in the dashboard call posts but the url will be called event
+        'public' => true, //add a space in the dashboard call posts but the url will be called event
         'labels' => array(
             'name' => 'Events', //to change post to event in the dashboard 
             'add_new_item' => 'Add a new event',
-            'edit_item' => 'Edit event', 
+            'edit_item' => 'Edit event',
             'all_items' => 'All Events',
-            'singular_name' =>'Event'
+            'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar' //https://developer.wordpress.org/resource/dashicons/#location
-         
+
     ));
     // To see more paremeters of register_post_type visit this web site 
     // https://developer.wordpress.org/reference/functions/register_post_type/ 
-    
+
     //IMPORTANT: Remember if you add a new parameter inside the array you need to go to the
-    // setting-permalink and save 
+    // setting-permalink and save
+    //****************************************************************************************************** */
+
+    // PROGRAM POST TYPE
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'), //always in plural 
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add a new program',
+            'edit_item' => 'Edit program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program',
+        ),
+        'menu_icon' => 'dashicons-awards'
+
+    ));
+     //IMPORTANT: Remember if you add a new parameter inside the array you need to go to the
+    // setting-permalink and save
 }
 add_action('init', 'university_post_types');
-?>
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -62,8 +62,16 @@ add_action('after_setup_theme', 'university_features'); //after_setup_theme -> t
 //
 
 function university_adjust_queries($query)
-{
-    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+{   
+    //This is to custom your archive program page 
+    if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+
+
+    }
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
         $today = date('Ymd');
         // $query->set('posts_per_page', '1');
         $query->set('meta_key', 'event_date');
