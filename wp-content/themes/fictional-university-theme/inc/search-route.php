c4d43332c44c108a201249571d0da12f31d75e81
Dean <deaneryfb@gmail.com>
2022-05-06 16:39:12 -0600

    Use keywords to search in URL, avoid SQL injection, multiple posts type searching - Organize the result according to the post type

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -11,21 +11,59 @@ function universityRegisterSearch()
     ));
 }
 
-function universitySearchResult()
+function universitySearchResult($data)
 {
     //Get the collections of posts 
-    $professors = new WP_Query(array(
-        'post_type' => 'professor'
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
+        's' => sanitize_text_field($data['term']) // (s) means search  | (sanitize_text_field) to avoid sql injection in the url    
     ));
-    $professorResults = array();
+    $results = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array()
 
-    while ($professors->have_posts()) {
-        $professors->the_post(); //the_post() gets the related data of the posts, title, content etc
-        array_push($professorResults, array(
-            'title' => get_the_title(), 
-            'permalink' => get_the_permalink()
-        ));//So in the second parameter we add the data we need to display 
+    );
+
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post(); //the_post() gets the related data of the posts, title, content etc
+        if (get_post_type() == 'post' or get_post_type() == 'page') {
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            )); //So in the second parameter we add the data we need to display 
+        }
+
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'program') {
+            array_push($results['programs'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'campus') {
+            array_push($results['campuses'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+        if (get_post_type() == 'event') {
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
     }
-    return $professorResults;
+    return $results;
     //    return $professors->posts; this displays everything 
 }
