0288d1cb8e46ac23a08931b94596deeb17d83868
Dean <deaneryfb@gmail.com>
2022-05-06 16:03:39 -0600

    Create Your Own Raw JSON Data

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -13,5 +13,19 @@ function universityRegisterSearch()
 
 function universitySearchResult()
 {
-    return 'Congratulations, you created a route.';
+    //Get the collections of posts 
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+    $professorResults = array();
+
+    while ($professors->have_posts()) {
+        $professors->the_post(); //the_post() gets the related data of the posts, title, content etc
+        array_push($professorResults, array(
+            'title' => get_the_title(), 
+            'permalink' => get_the_permalink()
+        ));//So in the second parameter we add the data we need to display 
+    }
+    return $professorResults;
+    //    return $professors->posts; this displays everything 
 }
