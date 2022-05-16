e2517797b0433f398727e371ad617032bb063ac2
Dean <deaneryfb@gmail.com>
2022-05-16 03:07:44 -0600

    Finishing Word Filter Plugin

--- wp-content/plugins/our-3rd-plugin-filter/index.php
+++ wp-content/plugins/our-3rd-plugin-filter/index.php
@@ -25,6 +25,27 @@ class OurWordFilterPlugin
     function __construct()
     {
         add_action('admin_menu', array($this, 'ourMenu'));
+        add_action('admin_init', array($this, 'ourSettings'));
+        if (get_option('plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic'));
+    }
+
+    function ourSettings()
+    {
+        // 2nd and 3rd display text labels 
+        add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+        register_setting('replacementFields', 'replacementText');
+        add_settings_field('replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+    }
+    function replacementFieldHTML()
+    { ?>
+        <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')) ?>">
+        <p class="description">Leave blank to simply remove the filtered words</p>
+        <?php }
+    function filterLogic($content)
+    {
+        $badWords = explode(',', get_option('plugin_words_to_filter'));
+        $badWordsTrimed = array_map('trim', $badWords);
+        return str_ireplace($badWordsTrimed, esc_html(get_option('replacementText', '***')), $content);
     }
 
     function ourMenu()
@@ -107,7 +128,18 @@ class OurWordFilterPlugin
 
     function optionsSubPage()
     { ?>
-        Hello word from options page
+        <div class="wrap">
+            <h1>Word filter Options</h1>
+            <!-- options.php -> default page of wordpress  -->
+            <form action="options.php" method="POST">
+                <?php
+                settings_errors();
+                settings_fields('replacementFields');
+                do_settings_sections('word-filter-options');
+                submit_button();
+                ?>
+            </form>
+        </div>
 <?php }
 }
 //We use variable to do not have conflict with another plugin
