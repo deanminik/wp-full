ec86f79df5fad082dd774e59e7ee08c8d7775bda
Dean <deaneryfb@gmail.com>
2022-05-15 18:38:43 -0600

    Counting words, characters and read time ready

--- wp-content/plugins/our-2nd-plugin/our-2nd-plugin.php
+++ wp-content/plugins/our-2nd-plugin/our-2nd-plugin.php
@@ -15,6 +15,52 @@ class WordCountAndTimePlugin
         //$this -> to use the class WordCountAndTimePlugin
         add_action('admin_menu', array($this, 'adminPage'));
         add_action('admin_init', array($this, 'settings'));
+
+        add_filter('the_content', array($this, 'ifWrap'));
+    }
+    //$content -> the_content
+    //1 or 0 -> true or false
+    function ifWrap($content)
+    {
+        if ((is_main_query() and is_single()) and
+            (get_option('wcp_wordCount', '1') or get_option('wcp_characterCount', '1') or get_option('wcp_timeCount', '1'))
+        ) {
+            // return $this->createHTML($content) . 'Hello from the statement there is at least one true'; 
+            return $this->createHTML($content);
+        }
+        // return $content . 'Hello from the statement there are only false';
+        return $content;
+    }
+
+    function createHTML($content)
+    {
+        $html = '<h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+        //Get word count once because both wordCount and read time will need it; 
+        if (get_option('wcp_wordCount', '1') or get_option('wcp_readTime', '1')) {
+
+            $wordCount = str_word_count(strip_tags($content));
+        }
+        if (get_option('wcp_wordCount', '1')) {
+            $html .= 'This post has ' . $wordCount . ' words.<br>';
+        }
+        if (get_option('wcp_characterCount', '1')) {
+            //strlen -> php function, len of a string 
+            $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+        }
+
+        if (get_option('wcp_readTime', '1')) {
+            //strlen -> php function, len of a string 
+            $html .= 'This post will take about ' . round($wordCount / 225) . ' minute(s) to read<br>';
+        }
+
+        $html .= '</p>';
+
+        if (get_option('wcp_location', '0') == '0') {
+            //up there
+            return $html . $content;
+        }
+        //Down there
+        return $content . $html;
     }
 
     function settings()
@@ -57,7 +103,7 @@ class WordCountAndTimePlugin
 
     function sanitizeLocation($input)
     {
-        if ($input != '0' and $$input != '1') {
+        if ($input != '0' and $input != '1') {
             add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or ending');
             // this is in case the user opens inspect elements end change the value. Useful to do not inject wrong selection 
             return get_option('wcp_location');
