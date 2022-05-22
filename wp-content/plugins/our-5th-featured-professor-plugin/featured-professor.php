1c5cc7ee091ea69493d5332e5b3ffc43de55cbe9
Dean <deaneryfb@gmail.com>
2022-05-22 11:55:16 -0600

    Display professor info on the frontend

--- wp-content/plugins/our-5th-featured-professor-plugin/featured-professor.php
+++ wp-content/plugins/our-5th-featured-professor-plugin/featured-professor.php
@@ -7,14 +7,19 @@
   Author URI: https://www.udemy.com
 */
 
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-class FeaturedProfessor {
-  function __construct() {
+require_once plugin_dir_path(__FILE__) . 'inc/generateProfessorHTML.php';
+
+class FeaturedProfessor
+{
+  function __construct()
+  {
     add_action('init', [$this, 'onInit']);
   }
 
-  function onInit() {
+  function onInit()
+  {
     wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
     wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
 
@@ -24,11 +29,16 @@ class FeaturedProfessor {
       'editor_style' => 'featuredProfessorStyle'
     ));
   }
-
-  function renderCallback($attributes) {
-    return '<p>We will replace this content soon.</p>';
+  //This function is put on frontend with the option option from the page editor 
+  function renderCallback($attributes)
+  {
+    if ($attributes['profId']) {
+      wp_enqueue_style('featuredProfessorStyle');
+      return generateProfessorHTML($attributes['profId']);
+    } else {
+      return NULL;
+    }
   }
-
 }
 
 $featuredProfessor = new FeaturedProfessor();
